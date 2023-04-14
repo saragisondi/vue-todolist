7f9c4d192113379bc665180ed2edb44e2bd4d0b5
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-14 15:42:10 +0200

    add markup

--- js/script.js
+++ js/script.js
@@ -3,7 +3,27 @@ const {createApp} = Vue;
 createApp({
 data(){
   return{
-    
+   lists:
+     [
+      {
+        text:'lista spesa',
+        done: false,
+      },
+      {
+        text:'lista',
+        done: true,
+      },
+      {
+        text:'spesa',
+        done: false,
+      }
+    ]
   }
-}
+},
+
+
+
+
+
+
 }).mount('#app')
