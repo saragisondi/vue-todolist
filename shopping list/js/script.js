7ad6fefd652df89c72e68dc574fd74da40fdf0fd
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-20 08:22:57 +0200

    add category

--- shopping list/js/script.js
+++ shopping list/js/script.js
@@ -3,8 +3,56 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-
-    lists:[]
+    lists:[
+      {
+        category:'Frutta e Verdura',
+        img:'img/frutta.png',
+        done:false
+      },
+      {
+        category:'Carne e Pesce',
+        img:'img/carne.png',
+        done:false
+      },
+      {
+        category:'Panetteria e Pasta',
+        img:'img/pane.png',
+        done:false
+      },
+      {
+        category:'Latte e Formaggi',
+        img:'img/latte.png',
+        done:false
+      },
+      {
+        category:'Bevande',
+        img:'img/bevande.png',
+        done:false
+      },
+      {
+        category:'Non Classificati',
+        img:'img/x.jpg',
+        done:false
+      },
+    ],
+    toDo: "",
     }
+},
+
+methods:{
+
+addNew(){
+  const addNewToDo = {
+    text: this.toDo,
+    done: false
   }
+  
+  this.lists.unshift(addNewToDo)
+  console.log(addNewToDo)
+  this.toDo="";
+}
+}
+
+
+
 }).mount('#app')
